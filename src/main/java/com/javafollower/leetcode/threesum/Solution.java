42a57439fdb8afd2f7d50ce2f98b55c3f54bc69c
lixin <283202734@qq.com>
2019-10-11 23:18:16 +0800

    给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。

--- src/main/java/com/javafollower/leetcode/threesum/Solution.java
+++ src/main/java/com/javafollower/leetcode/threesum/Solution.java
@@ -4,24 +4,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ */
 public class Solution {
 
-    /**
-     * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
-     * 注意：答案中不可以包含重复的三元组。
-     *
-     * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
-     *
-     * 满足要求的三元组集合为：
-     * [
-     *   [-1, 0, 1],
-     *   [-1, -1, 2]
-     * ]
-     *
-     * @param nums
-     * @return
-     */
-    public static List<List<Integer>> threeSum(int[] nums) {
+    private static List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> result = new ArrayList<>();
 
@@ -54,7 +51,6 @@ public class Solution {
                 }
             }
         }
-
 
         return result;
     }
