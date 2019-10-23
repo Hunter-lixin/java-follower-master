0e394858fb38182c16485dc9507f9ce7ea0152e8
lixin <283202734@qq.com>
2019-10-24 07:04:16 +0800

    给定一个二叉树，检查它是否是镜像对称的。

---
+++ src/main/java/com/javafollower/leetcode/isSymmetric/Solution.java
@@ -0,0 +1,34 @@
+package com.javafollower.leetcode.isSymmetric;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        return left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
+
+
+
---
+++ src/main/java/com/javafollower/leetcode/isSymmetric/TreeNode.java
@@ -0,0 +1,11 @@
+package com.javafollower.leetcode.isSymmetric;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
