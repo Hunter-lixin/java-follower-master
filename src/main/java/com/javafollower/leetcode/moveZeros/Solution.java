d8df2af67247f7f8a5a4094c82930223aa4a8b84
lixin <283202734@qq.com>
2019-11-10 05:46:24 +0800

    给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。

---
+++ src/main/java/com/javafollower/leetcode/moveZeros/Solution.java
@@ -0,0 +1,39 @@
+package com.javafollower.leetcode.moveZeros;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                if (i != j) {
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        Solution solution = new Solution();
+        solution.moveZeroes(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
