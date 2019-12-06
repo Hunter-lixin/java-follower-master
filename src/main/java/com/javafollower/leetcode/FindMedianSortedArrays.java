57a7ad9b5fada73086fc73d2dfd412dd182c98da
lixin <283202734@qq.com>
2019-12-06 16:40:44 +0800

    给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。找出中位数

--- src/main/java/com/javafollower/leetcode/FindMedianSortedArrays.java
+++ src/main/java/com/javafollower/leetcode/FindMedianSortedArrays.java
@@ -2,22 +2,17 @@ package com.javafollower.leetcode;
 
 /**
  * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
- *
  * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- *
  * 你可以假设 nums1 和 nums2 不会同时为空。
- *
+ * <p>
  * 示例 1:
- *
  * nums1 = [1, 3]
  * nums2 = [2]
- *
  * 则中位数是 2.0
+ * <p>
  * 示例 2:
- *
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- *
  * 则中位数是 (2 + 3)/2 = 2.5
  */
 public class FindMedianSortedArrays {
@@ -25,14 +20,15 @@ public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        int left = (n + m + 1) / 2;
-        int right = (n + m + 2) / 2;
 
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
         return (getKth(nums1, 0, m - 1, nums2, 0, n - 1, left)
                 + getKth(nums1, 0, m - 1, nums2, 0, n - 1, right)) * 0.5;
     }
 
-    public int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
         int length1 = end1 - start1 + 1;
         int length2 = end2 - start2 + 1;
 
@@ -56,6 +52,7 @@ public class FindMedianSortedArrays {
         } else {
             return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
         }
+
     }
 
 }
