02df2fc2ebf6e051707a524392828d411a9f057c
lixin <283202734@qq.com>
2020-01-03 05:57:24 +0800

    不同的路径

--- src/main/java/com/javafollower/leetcode/UniquePaths.java
+++ src/main/java/com/javafollower/leetcode/UniquePaths.java
@@ -30,11 +30,9 @@ public class UniquePaths {
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0) {
+                if (i == 0 || j == 0) {
                     dp[i][j] = 1;
-                } else if (j == 0) {
-                    dp[i][j] = 1;
-                }else {
+                } else {
                     dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
                 }
             }
